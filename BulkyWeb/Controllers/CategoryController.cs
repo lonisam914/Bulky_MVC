78983f23e3925a7fe1ae5303acd3e0bccceb2e8c
SamarthLoni <samarthloni1111.com>
2025-10-24 15:38:05 +0530

    Implimented Repository for Category Model

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
 using BulkyWeb.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,15 +7,15 @@ namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+			_categoryRepo = db;
         }
         public IActionResult Index()
         {
             List<Category> categories =  new List<Category>();
-            categories = _db.Categories.ToList();
+            categories = _categoryRepo.GetAll().ToList();
             return View(categories);
         }
 
@@ -31,8 +32,8 @@ namespace BulkyWeb.Controllers
             }
             if (ModelState.IsValid)   //check model property validations
             {
-				_db.Categories.Add(obj);
-				_db.SaveChanges();
+				_categoryRepo.Add(obj);
+				_categoryRepo.Save();
 				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index", "Category");
 			}	 
@@ -45,7 +46,7 @@ namespace BulkyWeb.Controllers
             {
               return NotFound();
             }
-            Category category = _db.Categories.Find(id);
+            Category category = _categoryRepo.Get(u => u.Id ==id);
             //other ways to impliment
             //Category? category1 = _db.Categories.FirstOrDefault(c => c.Id == id);
 			//Category? category2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
@@ -62,8 +63,8 @@ namespace BulkyWeb.Controllers
 
             if (ModelState.IsValid)   //check model property validations
 			{
-				_db.Categories.Update(obj);
-				_db.SaveChanges();
+				_categoryRepo.Update(obj);
+				_categoryRepo.Save();
 				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index", "Category");
 			}
@@ -76,7 +77,7 @@ namespace BulkyWeb.Controllers
 			{
 				return NotFound();
 			}
-			Category? category = _db.Categories.Find(id);
+			Category? category = _categoryRepo.Get(u => u.Id == id);
 			//other ways to impliment
 			//Category? category1 = _db.Categories.FirstOrDefault(c => c.Id == id);
 			//Category? category2 = _db.Categories.Where(c => c.Id == id).FirstOrDefault();
@@ -90,10 +91,10 @@ namespace BulkyWeb.Controllers
 		[HttpPost , ActionName("Delete")]
 		public IActionResult DeletePost(int? id)
 		{
-			Category? category = _db.Categories.Find(id);
+			Category? category = _categoryRepo.Get(u => u.Id == id);
 			if (category == null) { return NotFound() ; }
-			_db.Categories.Remove(category);
-			_db.SaveChanges();
+			_categoryRepo.Remove(category);
+			_categoryRepo.Save();
 			TempData["success"] = "Category created successfully";
 			return RedirectToAction("Index", "Category");
 		}
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,6 @@
 using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -13,7 +15,9 @@ namespace BulkyWeb
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>
-                (options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
